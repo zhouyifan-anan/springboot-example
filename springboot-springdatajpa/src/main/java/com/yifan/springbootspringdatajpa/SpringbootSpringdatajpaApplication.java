cd234070e8da83b7e679dd37e9e321d9aaf5fbd9
zhou-one-fan <18829716489@163>
2019-05-20 22:46:22 +0800

    springboot学习笔记整合springdata jpa

---
+++ springboot-springdatajpa/src/main/java/com/yifan/springbootspringdatajpa/SpringbootSpringdatajpaApplication.java
@@ -0,0 +1,13 @@
+package com.yifan.springbootspringdatajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSpringdatajpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootSpringdatajpaApplication.class, args);
+	}
+
+}
