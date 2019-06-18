10e28ebd840d488b64fc285b0d554e754ad63a1f
zhou-one-fan <18829716489@163>
2019-06-19 07:41:25 +0800

    springboot2.x整合ElasticSearch6.4.3实现结果高亮

--- springboot-elasticsearch/src/test/java/com/yifan/springbootelasticsearch/repository/PersonRepositoryTest.java
+++ springboot-elasticsearch/src/test/java/com/yifan/springbootelasticsearch/repository/PersonRepositoryTest.java
@@ -1,16 +1,26 @@
 package com.yifan.springbootelasticsearch.repository;
 
+import com.alibaba.fastjson.JSON;
 import com.yifan.springbootelasticsearch.SpringbootElasticsearchApplicationTests;
 import com.yifan.springbootelasticsearch.entity.Person;
+import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.fetch.subphase.highlight.HighlightBuilder;
+import org.elasticsearch.search.fetch.subphase.highlight.HighlightField;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
+import org.springframework.data.elasticsearch.core.SearchResultMapper;
+import org.springframework.data.elasticsearch.core.aggregation.AggregatedPage;
+import org.springframework.data.elasticsearch.core.aggregation.impl.AggregatedPageImpl;
 import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
 import org.springframework.data.elasticsearch.core.query.SearchQuery;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 /**
  * <B>文件名称：</B> <BR>
@@ -58,19 +68,58 @@ public class PersonRepositoryTest extends SpringbootElasticsearchApplicationTest
 
     @Test
     public void searchAll() {
+        String preTag = "<em>";//google的色值
+        String postTag = "</em>";
         String str = "天津";
         SearchQuery searchQuery = new NativeSearchQueryBuilder()
                 .withQuery(QueryBuilders.matchQuery("name", str))
-                .withFilter(QueryBuilders.matchPhraseQuery("id", "1"))
+                //.withQuery(QueryBuilders.matchQuery("city", str))
+                .withHighlightFields(new HighlightBuilder.Field("name").preTags(preTag).postTags(postTag),
+                                     new HighlightBuilder.Field("city").preTags(preTag).postTags(postTag))
                 .build();
 
-        Page<Person> person = personRepository.search(searchQuery);
-        System.out.println(person.getTotalElements());
+        //Page<Person> person = personRepository.search(searchQuery);
 
-        for (Person p : person) {
+
+        // 高亮字段
+        AggregatedPage<Person> people = elasticsearchTemplate.queryForPage(searchQuery, Person.class, new SearchResultMapper() {
+
+            @Override
+            public <T> AggregatedPage<T> mapResults(SearchResponse response, Class<T> clazz, Pageable pageable) {
+                List<Person> chunk = new ArrayList<>();
+                for (SearchHit searchHit : response.getHits()) {
+                    if (response.getHits().getHits().length <= 0) {
+                        return null;
+                    }
+
+                    String objJsonStr = searchHit.getSourceAsString();
+                    //todo 序列化为实体对象
+
+
+                    Person person1 = JSON.parseObject(objJsonStr, Person.class);
+                    //name or memoe
+                    HighlightField name = searchHit.getHighlightFields().get("name");
+                    if (name != null) {
+                        person1.setName(name.fragments()[0].toString());
+                    }
+                    HighlightField city = searchHit.getHighlightFields().get("city");
+                    if (city != null) {
+                        person1.setCity(city.fragments()[0].toString());
+                    }
+                    chunk.add(person1);
+                }
+                if (chunk.size() > 0) {
+                    return new AggregatedPageImpl<>((List<T>) chunk);
+                }
+                return null;
+            }
+        });
+
+        System.out.println(people.getTotalElements());
+
+        for (Person p : people) {
             System.out.println(p);
         }
-
     }
 
 
